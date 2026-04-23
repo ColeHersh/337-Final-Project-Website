299e70ba0715535e8430fee4df3be7d961be41f4
Name <email>
2026-04-22 21:40:36 -0700

    sevred createUser

--- modules/user.js
+++ modules/user.js
@@ -10,4 +10,8 @@ module.exports = function(app, db) {
     app.get(['/', '/login'], (req, res) => {
         res.sendFile(path.join(__dirname, '../html_files/login.html'));
     });
+
+    app.get('/createUser', (req, res) => {
+        res.sendFile(path.join(__dirname, '../html_files/createUser.html'));
+    });
 };
