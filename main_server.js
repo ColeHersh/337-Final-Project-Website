6088048740af3c3e896f458f552daae7ac699da7
Name <email>
2026-04-22 18:35:41 -0700

    intial setup of all files, folders, and packages,

---
+++ main_server.js
@@ -0,0 +1,6 @@
+/**
+ * This is the main server file for the movie review/watchlist website.
+ * All other js files should be linked here if any are used.
+ * Each module's code is in its own file to avoid merge conflicts
+ */
+
